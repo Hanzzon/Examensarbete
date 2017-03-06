cd1769396f62a405d489310fcbf8b86614b78afc
Hanzzon <Rasmus.Hansson93@hotmail.com>
2017-03-06 17:41:06 +0100

    Portal Added

---
+++ Examensarbete/Examensarbete/Assets/Scripts/EnterBuildBridge.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterBuildBridge : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnCollisionEnter(Collision _collision)
+	{
+		if (_collision.gameObject.name == "BuildBridge") {
+			SceneManager.LoadScene ("BuildBridge");
+		}
+	}
+}
