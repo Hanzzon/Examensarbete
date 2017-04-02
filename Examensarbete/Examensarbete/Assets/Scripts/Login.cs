df2e034e235caf7f8b0201cec2bdfe8f3d01cd02
supergajic <dropmedia@dropmedias-Mac-Pro.local>
2017-04-02 21:22:45 +0200

    Update UI

---
+++ Examensarbete/Examensarbete/Assets/Scripts/Login.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Login : MonoBehaviour {
+
+	public Text username;
+	public Text password;
+
+	public void LoginUser()
+	{
+		if (username.text.Contains ("100") && password.text.Contains ("123")) {
+			print ("Success!");
+			SceneManager.LoadScene ("Map");
+		}
+		else
+			print ("Fail!");
+	}
+}
---
+++ Examensarbete/Examensarbete/Assets/Scripts/Movement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public LayerMask playerMask;
+	public Transform groundCheck;
+
+	private Rigidbody2D rb;
+	private Animator anim;
+
+	private float speed = 100f;
+	private float jumpForce = 100f;
+
+	private float hInput = 0;
+	private float groundRadius = 0.2f;
+
+	public bool isGrounded = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+
+	void FixedUpdate()
+	{
+		isGrounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, playerMask);
+
+		if (!isGrounded)
+			anim.SetBool ("Jump", true);
+		else if(isGrounded)
+			anim.SetBool ("Jump", false);
+
+		Move (hInput);
+	}
+
+	public void Move(float horizontalInput)
+	{
+		Vector2 moveVel = rb.velocity;
+		moveVel.x = horizontalInput * speed;
+		rb.velocity = moveVel;
+
+		anim.SetFloat ("Speed", Mathf.Abs (horizontalInput));
+
+		if(horizontalInput < 0)
+			transform.localScale = new Vector3 (-100, 100, 1);
+		else if(horizontalInput > 0)
+			transform.localScale = new Vector3 (100, 100, 1);
+	}
+
+	public void StartMoving(float horizontalInput)
+	{
+		hInput = horizontalInput;
+	}
+
+	public void Jump()
+	{
+		if (isGrounded)
+			rb.velocity += jumpForce * Vector2.up;
+	}
+}
