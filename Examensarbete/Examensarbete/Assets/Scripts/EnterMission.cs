f72be79898310bc2e441cc814e198cb0c31ff47f
Hanzzon <Rasmus.Hansson93@hotmail.com>
2017-04-03 12:37:06 +0200

    Portal Collision

---
+++ Examensarbete/Examensarbete/Assets/Scripts/EnterMission.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterMission : MonoBehaviour {
+
+	private int levelIndex = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D _collision)
+	{
+		levelIndex = Random.Range (1, 3);
+		if (_collision.gameObject.name == "MissionPortal") {
+			if (levelIndex == 1) {
+				SceneManager.LoadScene ("BuildBridge");
+				print(levelIndex);
+			}
+			else if(levelIndex == 2)
+			{
+				SceneManager.LoadScene ("MissionDefeatEnemy");
+			}
+		}
+	}
+}
